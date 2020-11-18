667a12f1c9b9c43140bb997584702162762d7f58
Murugan D <yasmuru@gmail.com>
2020-11-18 12:08:05 +0530

    Update to include soft deletion package

--- app/models.py
+++ app/models.py
@@ -4,54 +4,7 @@ from django.db import models
 from django_extensions.db.fields import RandomCharField
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-
-
-class SoftDeletionQuerySet(models.QuerySet):
-
-    def delete(self):
-        return super(SoftDeletionQuerySet, self).update(deleted_at=timezone.now())
-
-    def hard_delete(self):
-        return super(SoftDeletionQuerySet, self).delete()
-
-    def alive(self):
-        return self.filter(deleted_at=None)
-
-    def dead(self):
-        return self.exclude(deleted_at=None)
-
-
-class SoftDeletionManager(models.Manager):
-
-    def __init__(self, *args, **kwargs):
-        self.alive_only = kwargs.pop('alive_only', True)
-        super(SoftDeletionManager, self).__init__(*args, **kwargs)
-
-    def get_queryset(self):
-        if self.alive_only:
-            return SoftDeletionQuerySet(self.model).filter(deleted_at=None)
-        return SoftDeletionQuerySet(self.model)
-
-    def force_delete(self):
-        return self.get_queryset().delete()
-
-
-class SoftDeletionModel(models.Model):
-    deleted_at = models.DateTimeField(blank=True, null=True)
-
-    objects = SoftDeletionManager()
-    all_objects = SoftDeletionManager(alive_only=False)
-
-    class Meta:
-        abstract = True
-
-    def delete(self):
-        self.deleted_at = timezone.now()
-        self.save()
-
-    def force_delete(self):
-        super(SoftDeletionModel, self).delete()
-
+from django_soft_deletion.models import SoftDeletionModel
 
 class UserManager(BaseUserManager):
     """Define a model manager for User model with no username field."""
